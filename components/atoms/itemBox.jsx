bd56c825007d50b05178e2e438c8006a678ca067
linkavier <linkjavier@hotmail.com>
2023-10-15 16:08:59 -0500

    Se arregla Opacity

--- components/atoms/itemBox.jsx
+++ components/atoms/itemBox.jsx
@@ -6,16 +6,18 @@ const ItemBox = ({ element }) => {
     return (
     <Link href={`/producto/${queryString}`}>
         <div 
-            className="xl:hover:bg-black xl:hover:opacity-75 bg-cover bg-center shadow-2xl rounded-xl"
+            className="bg-cover bg-center shadow-2xl rounded-xl box-border"
             style={{backgroundImage: `url(${element.data.plato_imagen.url})`,}}>
-            <div className="rounded-xl box-border h-32 border-4 text-white font-viking">
-                <div className=''>
-                    <div className='font-viking'>
-                        {element.data.plato_titulo}
+                <div className='xl:hover:bg-black xl:hover:opacity-75 rounded-xl'>
+                    <div className="rounded-xl  h-32 border-4 text-white font-viking">
+                        <div className='p-1'>
+                            <div className='font-viking flex'>
+                                {element.data.plato_titulo}
+                            </div>
+                        </div>
+                        
                     </div>
                 </div>
-                
-            </div>
       </div>
     </Link>
     );
